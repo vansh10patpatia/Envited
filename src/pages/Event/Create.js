1f684480fdcc51560bddc11fa43716ea04db98b7
Vansh Patpatia <68200019+vansh10patpatia@users.noreply.github.com>
2022-10-04 00:35:56 +0530

    changes in event

--- src/pages/Event/Create.js
+++ src/pages/Event/Create.js
@@ -1,7 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import CreateImg from "../../assets/images/create-event.svg";
 
 const Create = () => {
+
+    const navigate = useNavigate();
+    const [eventDetails , setEventDetails] = useState({
+        eventName: "",
+        eventHost: "",
+        startDate: "",
+        endDate : "",
+        location : "",
+        image : "",
+    })
+
+    const handleCreateEvent = () => {
+        navigate("/event", { state: eventDetails });
+    }
+
   return (
     <>
         <div className="create-event-wrapper">
@@ -12,28 +28,41 @@ const Create = () => {
                     <div className="input-container">
                         <div className="inputs">
                             {/* <label>Event Name</label> */}
-                            <input type={"text"} placeholder={"Event Name"}  />
+                            <input type={"text"} placeholder={"Event Name"} 
+                                onChange={(e)=>{setEventDetails({...eventDetails, eventName: e.target.value})}}  
+                            />
                         </div>
                         <div className="inputs">
                             {/* <label>Host Name</label> */}
-                            <input type={"text"} placeholder={"Host Name"}  />
+                            <input type={"text"} placeholder={"Host Name"}
+                                onChange={(e)=>{setEventDetails({...eventDetails, eventHost: e.target.value})}}
+                            />
                         </div>
                         <div className="start-date">
                             {/* <label>Start Date</label> */}
-                            <input type={"date"} placeholder={"Start Date"}  />
+                            <input type={"date"} placeholder={"Start Date"}
+                                onChange={(e)=>{setEventDetails({...eventDetails, startDate: e.target.value})}}
+                            />
                         </div>
                         <div className="end-date">
                             {/* <label>End Date</label> */}
-                            <input type={"date"} placeholder={"End Date"}  />
+                            <input type={"date"} placeholder={"End Date"} 
+                                onChange={(e)=>{setEventDetails({...eventDetails, endDate: e.target.value})}}
+                            />
                         </div>
                         <div className="inputs">
                             {/* <label>Location</label> */}
-                            <input type={"text"} placeholder={"Location"}  />
+                            <input type={"text"} placeholder={"Location"}  
+                                onChange={(e)=>{setEventDetails({...eventDetails, location: e.target.value})}}
+                            />
                         </div>
-                        <input type={"file"} placeholder={"Image"}  />
+                        <input type={"file"} placeholder={"Image"}  
+                            accept="image/*"
+                            onChange={(e)=>{setEventDetails({...eventDetails, image: URL.createObjectURL(e.target.files[0])})}}
+                        />
                     </div>
                     <div className="button-container">
-                        <button className="create-event-btn">
+                        <button className="create-event-btn" onClick={handleCreateEvent}>
                              Create 🎉
                         </button>
                     </div>
--- src/pages/Event/index.js
+++ src/pages/Event/index.js
@@ -4,21 +4,26 @@ import Location from "../../assets/images/location.svg";
 import Cake from "../../assets/images/Cake.svg";
 import Arrow from "../../assets/images/arrow.svg";
 
+import { useLocation } from 'react-router-dom';
+import moment from 'moment/moment';
+
 const Event = () => {
+    const {state} = useLocation();
+    console.log(state);
   return (
     <>
         <div className="event-page-container">
             <div className="event-text-cont">
                 <div className="text-details">
-                    <h2>Birthday Bash</h2>
-                    <p>Hosted by <b>Elysia</b></p>
+                    <h2>{state?.eventName ? state?.eventName : 'Birthday Bash'}</h2>
+                    <p>Hosted by <b>{state?.eventHost ? state?.eventHost : 'Elysia'}</b></p>
                     <div className="event-details-cont">
                         <div className="event-details">
                             <img src={Calendar} />
                             <div className="sub-info">
                                 <p>
-                                    <span>18 August 6:00PM</span><br />
-                                    <span className="sub-detail-info">to <b>19 August 1:00PM</b> UTC +10</span>
+                                    <span>{state?.startDate ? moment(state?.startDate).format("LL") :('18 August 6:00PM')}</span><br />
+                                    <span className="sub-detail-info">to {state?.endDate ? moment(state?.endDate).format("LL") :(<b>19 August 6:00PM</b>)} UTC +10</span>
                                 </p>
                             </div>
                             <div className="arrow-img">
@@ -42,7 +47,7 @@ const Event = () => {
             </div>
             <div className="event-image-cont">
                 <div>
-                    <img src={Cake} />
+                    <img src={state?.image ? state.image : Cake} />
                 </div>
             </div>
         </div>
