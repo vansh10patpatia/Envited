e7f78effb9d853360b817448006a151d521ab0e4
Vansh Patpatia <68200019+vansh10patpatia@users.noreply.github.com>
2022-10-04 00:52:28 +0530

    home page responsive

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,23 +1,33 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 import LandingPage from "../../assets/images/Landing-Page.svg";
 
 const Home = () => {
+    const navigate = useNavigate();
   return (
     <>
         <div className="home-page-container">
             <div className="image-container">
-                <img src={LandingPage} />
+                <div>
+                    <img src={LandingPage} />
+                    <div className="button-container button-screen">
+                        <button className="create-event-btn" onClick={()=>{navigate("/create")}}>
+                            🎉 Create my event
+                        </button>
+                    </div>
+                </div>
+                
             </div>
             <div className="text-container">
-                <div>
+                <div className='text-align-center'>
                     <h2>
-                        Imagine if <span className="snapchat">Snapchat</span><br /> had events.
+                        Imagine if <br /><span className="snapchat">Snapchat</span><br /> had events.
                     </h2>
                     <p>
                         Easily host and share events with your friends across any social media.
                     </p>
-                    <div className="button-container">
-                        <button className="create-event-btn">
+                    <div className="button-container button-desktop">
+                        <button className="create-event-btn" onClick={()=>{navigate("/create")}}>
                             🎉 Create my event
                         </button>
                     </div>
